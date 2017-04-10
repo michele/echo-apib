7a1cc8a94802b4ba0c3ec56f7da6103833915ac6
Michele Finotto <m@finotto.org>
2017-04-10 11:25:33 +0200

    Refactored

--- apib.go
+++ apib.go
@@ -1,12 +1,9 @@
-package echo_apib
+package apib
 
 import (
 	"bufio"
-	"bytes"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 	"regexp"
 	"strings"
@@ -141,105 +138,6 @@ func Group(group string) {
 
 func Name(name string) {
 	currentName = name
-}
-
-func ApibGenerator(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		if recording && currentGroup != "" && currentName != "" {
-			bodyBytes, _ := ioutil.ReadAll(c.Request().Body)
-			c.Request().Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
-			request := Request{
-				URI:     c.Request().URL.RequestURI(),
-				Method:  c.Request().Method,
-				Body:    string(bodyBytes),
-				Headers: make(map[string]string),
-				Params:  make(map[string][]string),
-			}
-			for k, v := range c.Request().Header {
-				request.Headers[k] = v[0]
-			}
-
-			for k, v := range c.QueryParams() {
-				request.Params[k] = v
-			}
-
-			if len(params(c)) > 0 {
-				for k, v := range params(c) {
-					request.Params[k] = []string{v}
-					request.URI = strings.Replace(request.URI, "/"+v, "/{"+k+"}", 1)
-				}
-			}
-			call := Call{
-				Group:   currentGroup,
-				Name:    currentName,
-				Request: request,
-			}
-			res := c.Response()
-			rw := res.Writer
-			w := NewWriter(rw)
-			c.Response().Writer = w
-			err := next(c)
-			var response Response
-			if err == nil {
-				response = Response{
-					Body:       w.Body(),
-					StatusCode: c.Response().Status,
-					Headers:    make(map[string]string),
-				}
-			}
-			for k, v := range c.Response().Header() {
-				response.Headers[k] = v[0]
-			}
-			call.Response = response
-			if calls[currentGroup] == nil {
-				calls[currentGroup] = Resources{}
-			}
-			rss := calls[currentGroup][request.URI]
-			if rss.Calls == nil {
-				rss.Calls = []Call{}
-				rss.Params = make(map[string][]string)
-			}
-			rss.Params = request.Params
-			rss.Calls = append(rss.Calls, call)
-			calls[currentGroup][request.URI] = rss
-			currentName = ""
-			currentGroup = ""
-			return nil
-		}
-		return next(c)
-	}
-}
-
-type Writer struct {
-	http.ResponseWriter
-	text        string
-	wroteHeader bool
-}
-
-func NewWriter(res http.ResponseWriter) *Writer {
-	return &Writer{
-		ResponseWriter: res,
-	}
-}
-
-func (aw *Writer) Write(b []byte) (int, error) {
-	if !aw.wroteHeader {
-		aw.WriteHeader(http.StatusOK)
-	}
-	aw.text = aw.text + string(b)
-	return aw.ResponseWriter.Write(b)
-}
-
-func (aw *Writer) WriteHeader(code int) {
-	aw.ResponseWriter.WriteHeader(code)
-	if aw.wroteHeader {
-		return
-	}
-	aw.wroteHeader = true
-}
-
-func (aw *Writer) Body() string {
-	return aw.text
 }
 
 func params(c echo.Context) map[string]string {
---
+++ writer.go
@@ -0,0 +1,39 @@
+package apib
+
+import "net/http"
+
+type Writer struct {
+	http.ResponseWriter
+	text        string
+	wroteHeader bool
+}
+
+func NewWriter(res http.ResponseWriter) *Writer {
+	return &Writer{
+		ResponseWriter: res,
+	}
+}
+
+func (aw *Writer) Write(b []byte) (int, error) {
+	if !aw.wroteHeader {
+		aw.WriteHeader(http.StatusOK)
+	}
+	aw.text = aw.text + string(b)
+	return aw.ResponseWriter.Write(b)
+}
+
+func (aw *Writer) WriteHeader(code int) {
+	aw.ResponseWriter.WriteHeader(code)
+	if aw.wroteHeader {
+		return
+	}
+	aw.wroteHeader = true
+}
+
+func (aw *Writer) Header() http.Header {
+	return aw.ResponseWriter.Header()
+}
+
+func (aw *Writer) Body() string {
+	return aw.text
+}
